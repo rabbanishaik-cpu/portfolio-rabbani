17e5c007d2a56991a3a6b487819cfed13660dabc
rabbanishaik-cpu <rabbanishaik998@gmail.com>
2025-05-20 01:09:11 +0530

    fix-vercel delployment errors fix

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,12 @@
-"use client"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
+"use client";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ReactNode } from "react";
+import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes";
+
+interface ThemeProviderProps extends Omit<NextThemeProviderProps, "children"> {
+  children: ReactNode;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
